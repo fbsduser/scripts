b93fdee23717e1734c362c5815ffea04e442de53
t4z3v4r3d <porfi1290@gmail.com>
2021-05-18 14:08:51 +0430

    Update FBSD-Upgrade.sh

--- FBSD-Upgrade.sh
+++ FBSD-Upgrade.sh
@@ -10,9 +10,11 @@ add="https://svn.freebsd.org/base/stable/12"
 cd $path
 
 kern(){
-echo "Kernel Upgrade proc ..."
+clear
+echo -e "\033[1;31m \033[1;3f Kernel Upgrade proc ... \033[2;3f\033[0m"
 krn_name="`uname -v|cut -d "/" -f 8| cut -d " " -f 4`"
-cd /usr/src/ &&  make buildkernel KERNCONF=$krn_name && make installkernel KERNCONF=$krn_name && exit
+#cd /usr/src/ &&  make buildkernel KERNCONF=$krn_name && make installkernel KERNCONF=$krn_name && exit
+exit
 }
 
 up(){
@@ -22,10 +24,10 @@ up(){
     $command cleanup $path
     $command resolved $path ; $command cleanup $path 
     $command co $add $path && res=$?
-	[ "$res" -eq "0" ] && echo Done && kern && exit 
-	[ "$res" !="0" ] && $command resolved $path ; $command cleanup $path 
+        [ "$res" -eq "0" ] && (echo Done ; kern ; exit )
+        [ "$res" != "0" ] && $command resolved $path ; $command cleanup $path 
     let n++
-  up
+        [ "$res" -eq "0" ]&& exit ||   up
 }
 
 up
